7b0d62af3665eea8b71b3ceee92e70dd07ea7890
Anirudha <anirudhapatel.skumar@gmail.com>
2026-03-16 15:34:34 -0700

    new commit 2

--- src/GameObject.java
+++ src/GameObject.java
@@ -23,11 +23,31 @@ public class GameObject {
     }
 
     // TODO 1: Write getter methods for x, y, and hitbox.
+
+    public double getX(){
+        return x;
+    }
+    public double getY(){
+        return y;
+    }
+    public Rectangle getHitBox(){
+        return hitbox;
+    }
     
 
     // TODO 2: Write setter methods for x and y. 
     // HINT: When you change x or y, you MUST also update the hitbox!
     // Example: hitbox.setPosition((int) newX, (int) newY);
+
+    public void setX(double x){
+        this.x = x;
+        hitbox.setPosition((int)x, (int)y);
+    }
+
+    public void setY(double y){
+        this.y = y;
+        hitbox.setPosition((int)x, (int)y);
+    }
 
 
     /**
