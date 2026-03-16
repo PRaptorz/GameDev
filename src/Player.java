0b5d62adac999f96b182a1807679e9c9c2b5b3ad
Anirudha <anirudhapatel.skumar@gmail.com>
2026-03-16 15:34:12 -0700

    new commit

---
+++ src/Player.java
@@ -0,0 +1,8 @@
+public class Player extends GameObject{
+
+    public Player(double x, double y, int width, int height, int speed) {
+        super(x, y, 50, 50, 200);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
